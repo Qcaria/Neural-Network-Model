852e7193e9feed0b252e5d7c71cb95be933db8d9
Qcaria <38281560+Qcaria@users.noreply.github.com>
2018-04-19 12:43:32 +0200

    Add load_image

--- dataload.py
+++ dataload.py
@@ -1,7 +1,9 @@
 from mnist import MNIST
 import numpy as np
+import scipy
+from scipy import ndimage
 
-def load_set(set):
+def load_mnistset(set):
 
     mndata = MNIST('samples')
     if set == "train":
@@ -24,4 +26,10 @@ def load_set(set):
     return X, Y
 
 
+def load_image(image_name, pix_size):
+    fname = "samples/" + image_name
+    image = np.array(ndimage.imread(fname, flatten=True))
+    my_image = scipy.misc.imresize(image, size=(pix_size, pix_size)).reshape((pix_size*pix_size, 1))
+    my_image = my_image / 255.
+    return my_image
 
